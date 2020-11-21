69a84dc6c3aaee9012472d519c061e8c726e04aa
marcoref3at <marco_refaat@hotmail.com>
2020-11-22 01:16:32 +0200

    failed Login

---
+++ src/components/Auth/LoginRegister.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md column">
+      <q-banner class="bg-grey-3">
+        <template v-slot:avatar>
+          <q-icon name="login" color="primary" />
+        </template>
+        Login to access your AIACTIVE platform!
+      </q-banner>
+      <q-input color="teal" outlined v-model="text" label="Username" />
+      <q-input
+        color="teal"
+        outlined
+        v-model="password"
+        label="Password"
+        :type="isPwd ? 'password' : 'text'"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+      <q-btn label="Login" color="primary" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+      password: "",
+      isPwd: true,
+      email: "",
+      search: "",
+      tel: "",
+      url: "",
+      time: "",
+      date: ""
+    };
+  }
+};
+</script>
--- src/store/module-example/index.js
+++ src/store/module-example/index.js
@@ -1,12 +1,50 @@
-import state from './state'
-import * as getters from './getters'
-import * as mutations from './mutations'
-import * as actions from './actions'
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
 
-export default {
-  namespaced: true,
-  getters,
+const state = {
+
+};
+const mutations = {
+ 
+};
+const actions = {
+  loginUser({dispatch}, payload) {
+		Loading.show()
+		setTimeout(() => {
+			return new Promise((resolve, reject) => {
+				let host = config.API_URL + '/login'
+	
+				Axios.post(host, {
+					email: payload.email,
+					password: payload.password
+				})
+					.then(response => {
+						let userAuthData = response.data
+						dispatch('handleAuthStateChange', userAuthData)
+					})
+					.catch(error => {
+
+						if (error.message == 'Network Error'){
+							showErrorMessage('Server Offline')
+							return
+						}
+
+						if(error.response.status === 401) {
+							showErrorMessage(error.response.data.message)
+							return
+						}
+						else if(error.response.status === 404) {
+							showErrorMessage(error.response.data.message)
+							return
+						}
+					})
+			})
+		}, 500);
+	},
+};
+export default new Vuex.Store({
+  state,
   mutations,
   actions,
-  state
-}
+})
