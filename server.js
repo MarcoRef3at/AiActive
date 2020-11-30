8d4f8b5161ebcbe949f125f37532f06748aa464d
MarcoRef3at <Marco_Refaat@hotmail.com>
2020-11-30 18:08:38 +0200

    Express Initial Setup

--- server.js
+++ server.js
@@ -1,66 +1,13 @@
-const { stat } = require('fs');
-const http = require('http');
-const todos = [
-  { id: 1, text: 'Todo 1' },
-  { id: 2, text: 'Todo 2' },
-  { id: 3, text: 'Todo 3' },
-];
-const server = http.createServer((req, res) => {
-  //header///////////////////////////////
-  //   const { headers, url, method } = req;
-  //   console.log(headers, url, method);
-  //   console.log(req.headers.authorization);
-  //   res.statusCode=404;
-  //   res.setHeader('Content-Type', 'application/json');
-  //   res.setHeader('X-Powered-By', 'Node.js');
-  //   res.writeHead(404, {
-  //     'Content-Type': 'application/json',
-  //     'X-Powered-By': 'Node.js',
-  //   });
+const express = require('express');
+const dotenv = require('dotenv');
 
-  //body///////////////////////////////
-  const { method, url } = req;
-  let body = [];
-  req
-    .on('data', (chunk) => {
-      body.push(chunk);
-    })
-    .on('end', () => {
-      body = Buffer.concat(body).toString();
+//load env vars
+dotenv.config({ path: './config/config.env' });
 
-      let status = 404;
-      const response = {
-        success: false,
-        data: null,
-        error: null,
-      };
-      if (method === 'GET' && url === '/todos') {
-        status = 200;
-        response.success = true;
-        response.data = todos;
-      } else if (method === 'POST' && url === '/todos') {
-        const { id, text } = JSON.parse(body);
-        if (!id || !text) {
-          status = 400;
-          response.error = 'please add id and text';
-        } else {
-          todos.push({ id, text });
-          status = 201;
-          response.success = true;
-          response.data = todos;
-        }
-      }
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-      //header///////////////////
-      res.writeHead(status, {
-        'Content-Type': 'application/json',
-        'X-Powered-By': 'Node.js',
-      });
-      //response////////////////////
-      res.end(JSON.stringify(response));
-    });
-});
-
-const PORT = 5000;
-
-server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
