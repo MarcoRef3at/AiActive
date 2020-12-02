d34d7acd0521445272671c131e7a9434bcadc24e
MarcoRef3at <Marco_Refaat@hotmail.com>
2020-12-03 00:14:29 +0200

    User Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async');
 const Errorresponse = require('../utils/errorResponse');
 
 // @desc        Register user
-// @route       GET /api/v1/auth/register
+// @route       POST /api/v1/auth/register
 // @access      Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
@@ -14,5 +14,35 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role,
   });
-  res.status(200).json({ success: true });
+
+  //Create Token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
+// @desc        Login user
+// @route       POST /api/v1/auth/login
+// @access      Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  
+  // Validate email & Password
+  if (!email || !password) {
+    return next(new Errorresponse('Please Provide an Email and Password', 400));
+  }
+  
+  //check for user
+  const user = await User.findOne({ email }).select('+password');
+  if(!user){
+    return next(new Errorresponse('Invalid Credentials', 401));
+  }
+  //Check if password matches
+  const isMatch = await user.matchPassword(password);
+  if(!isMatch){
+    return next(new Errorresponse('Invalid Credentials', 401));
+  }
+  //Create Token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -32,10 +34,24 @@ const UserSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
 //Encrypt password before save using bcrypt
 UserSchema.pre('save', async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+//Sign JWT and return **TOKEN**
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+//Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 //other than module exports .. it creates the collection in the database adding 's' to it's name
 module.exports = mongoose.model('User', UserSchema);
