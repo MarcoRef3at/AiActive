af029a10d2d7f33277b6506a70890901dd929bb4
MarcoRef3at <Marco_Refaat@hotmail.com>
2020-12-11 20:16:40 +0200

    cookies router-auth

--- src/boot/router-auth.js
+++ src/boot/router-auth.js
@@ -1,7 +1,12 @@
-import { LocalStorage } from "quasar";
+//Cookies Import
+import Vue from "vue";
+import VueCookies from "vue-cookies";
+Vue.use(VueCookies);
+Vue.$cookies.config("7d");
+
 export default ({ router }) => {
   router.beforeEach((to, from, next) => {
-    let loggedIn = LocalStorage.getItem("loggedIn");
+    let loggedIn = $cookies.isKey('token');
     if (!loggedIn && to.path !== "/auth") {
       next("/auth");
     } else {
