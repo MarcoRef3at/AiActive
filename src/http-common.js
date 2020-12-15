4c2f45622b98655613d4b8b6c4e6596c36b615a0
MarcoRef3at <Marco_Refaat@hotmail.com>
2020-12-16 00:00:17 +0200

    Revert "Merge remote-tracking branch 'origin/master' into Modified"
    This reverts commit 90e5cad91e75322cbb32cf0c9d2ae51ad13621d0, reversing
    changes made to ec73075a4fad74348d1bd52ab41b8e5160a1d59e.

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.1.99:5000/api/v1",
+  baseURL: "http://localhost:5000/api/v1",
   headers: {
     "Content-type": "application/json"
   },
