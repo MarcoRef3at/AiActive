d1705bca7fc3d76a0dcf8545783286722833cf45
MarcoRef3at <Marco_Refaat@hotmail.com>
2021-02-12 14:48:27 +0200

    ok

--- src/components/Settings/UsersSettings/Modals/EditUser.vue
+++ src/components/Settings/UsersSettings/Modals/EditUser.vue
@@ -83,7 +83,7 @@ export default {
         if (this.resetPassword) {
           this.user.resetPassword = true;
         }
-
+        //todo: filter payload before send
         this.updateUser(this.user).finally(() => {
           this.loading = false;
         });
